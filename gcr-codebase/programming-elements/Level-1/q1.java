028be451e4c499ec52fa8d3c7a700cea944f0989
ShubhamChaudhary0 <chaudharyshubham1947@gmail.com>
2026-06-09 09:24:48 +0530

    assignment completed

---
+++ gcr-codebase/programming-elements/Level-1/q1.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int DOB = 2000;
+        int year = 2024;
+        int age = year - DOB;
+
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
